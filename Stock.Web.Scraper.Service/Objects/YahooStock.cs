37fedc65412ac485e46d93c3a9e019329354cff9
kepatotorica <kepatoto@gmail.com>
2022-02-23 16:05:55 -0700

    : got the scraper working

---
+++ Stock.Web.Scraper.Service/Objects/YahooStock.cs
@@ -0,0 +1,27 @@
+﻿using HtmlAgilityPack;
+using Stock.Web.Scraper.Service.ValuesForScraping;
+using System;
+using System.Linq;
+
+namespace Stock.Web.Scraper.Service.Objects
+{
+    public class YahooStock
+    {
+        public string Ticker { get; set; }
+        public DateTime DateAdded { get; set; }
+        public decimal? Price { get; set; }
+
+        public YahooStock(string ticker = null, DateTime? dateAdded = null)
+        {
+            Ticker = ticker;
+            DateAdded = dateAdded ?? DateTime.UtcNow;//TODOASDF should we subtract a day here?
+        }
+
+        public void SetPriceToOpen()
+        {
+            HtmlDocument doc = new HtmlWeb().Load($"https://finance.yahoo.com/quote/{Ticker}?p={Ticker}");
+
+            Price = Decimal.Parse(doc.DocumentNode.SelectNodes(ScraperInfo.StockPageIds.Open).First().InnerHtml);
+        }
+    }
+}
---
+++ Stock.Web.Scraper.Service/ValuesForScraping/ScraperInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace Stock.Web.Scraper.Service.ValuesForScraping
+{
+    public static class ScraperInfo
+    {
+        public static class StockPageIds
+        {
+            public static string Open => "//td[@data-test='OPEN-value']";
+        }
+
+        public static class ScreenerPageIds
+        {
+            public static string RowNames => "//tbody/tr/td/a";
+            public static string RowPrice => "//tbody/tr/td/fin-streamer[@data-field='regularMarketPrice']";
+        };
+
+        public static readonly List<(string title, string url)> Screeners = new()
+        {
+            new("Big Drop Large Cap", "https://finance.yahoo.com/screener/unsaved/894b6531-8650-4386-9038-86663c38712f"),
+        };
+    }
+}
